f145734251a3ca2e3accd1bca6ee3c66425810c7
Pietro <pietromaffepena@gmail.com>
2024-12-03 20:31:47 -0300

    historia

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function mudarImagem(direcao) {
   const carrossel = document.querySelector('.carrossel');
   carrossel.style.transform = `translateX(-${indiceAtual * 100}%)`;
 }
+
+function mudarImagem2(direcao) {
+  const imagens = document.querySelectorAll('.carrossel2 img');
+  const totalImagens = imagens.length;
+
+  indiceAtual += direcao;
+
+  if (indiceAtual < 0) {
+    indiceAtual = totalImagens - 1;
+  } else if (indiceAtual >= totalImagens) {
+    indiceAtual = 0;
+  }
+
+  const carrossel = document.querySelector('.carrossel2');
+  carrossel.style.transform = `translateX(-${indiceAtual * 100}%)`;
+}
+
